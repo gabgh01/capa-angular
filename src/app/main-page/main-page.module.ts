8198b6a0bdd3998fc34b56c9e532bccd487d4137
Gabriel Galvan <gabgh@hotmail.com>
2022-04-07 18:02:11 -0500

    creacion de main-page module

---
+++ src/app/main-page/main-page.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MainComponent } from './main/main.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [MainComponent],
+  imports: [CommonModule],
+})
+export class MainPageModule {}
